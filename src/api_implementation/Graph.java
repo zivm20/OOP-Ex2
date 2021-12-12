f0419dbd3b8e9643d26db503d5d0daf73f8eb478
Ziv Morgan <ziv.morgan@gmail.com>
2021-12-12 16:52:34 +0200

    fixed interators and finished tests

--- src/api_implementation/Graph.java
+++ src/api_implementation/Graph.java
@@ -11,6 +11,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Consumer;
 
 import com.google.gson.*;
 
@@ -54,13 +55,14 @@ public class Graph implements DirectedWeightedGraph {
 	
 	
 	
-	HashMap<Integer, NodeStructure> nodes;
-	LinkedList<NodeData> nodeList;
-	LinkedList<EdgeData> edgeList;
-	HashMap<Integer, Collection<EdgeData>> edgeMap;
-	boolean graphChanged;
-	int edgeSize;
-	
+	private HashMap<Integer, NodeStructure> nodes;
+	private int edgeSize;
+	//detect what node gets changed
+	private HashMap<Integer, Integer> lastNodeChange;
+	//detects changes in the amount of nodes
+	private int lastGraphChange;
+	//detects changes in the amount of edges
+	private int lastEdgeChange;
 	
 	public Graph(List<NodeData> nodeList, List<EdgeData> edgeList) {
 		init(nodeList,edgeList);
@@ -72,21 +74,20 @@ public class Graph implements DirectedWeightedGraph {
 	 */
 	public Graph(DirectedWeightedGraph g) {
 		this.nodes = new HashMap<Integer, NodeStructure>();
-		this.edgeMap = new HashMap<Integer, Collection<EdgeData>>();
-		this.graphChanged = false;
 		this.edgeSize = 0;
+		this.lastGraphChange = 0;
+		this.lastNodeChange = new HashMap<Integer, Integer>();
 		
 		for(Iterator<NodeData> n = g.nodeIter(); n.hasNext(); ) {
 			this.nodes.put(n.next().getKey(), new NodeStructure(n.next()));
+			lastNodeChange.put(n.next().getKey(),0);
 		}
 		for(Iterator<EdgeData> e = g.edgeIter(); e.hasNext(); ) {
 			this.nodes.get(e.next().getSrc()).addChild(e.next().getDest(),e.next());
 			this.nodes.get(e.next().getDest()).addParent(e.next().getSrc(),e.next());
 			this.edgeSize++;
 		}
-		for(Map.Entry<Integer, NodeStructure> entry: this.nodes.entrySet()) {
-			this.edgeMap.put(entry.getKey(), entry.getValue().getChildren().values());
-		}
+		
 	}
 	/**
 	 * Constructor from json file
@@ -138,23 +139,20 @@ public class Graph implements DirectedWeightedGraph {
 	 */
 	private void init(List<NodeData> nodeList, List<EdgeData> edgeList) {
 		this.nodes = new HashMap<Integer, NodeStructure>();
-		this.edgeMap = new HashMap<Integer, Collection<EdgeData>>();
-		this.edgeList = new LinkedList<EdgeData>(edgeList);
-		this.nodeList = new LinkedList<NodeData>(nodeList);
-		this.graphChanged = false;
-		this.edgeSize = 0;
 		
+		this.edgeSize = 0;
+		this.lastGraphChange = 0;
+		this.lastNodeChange = new HashMap<Integer, Integer>();
 		for(NodeData n: nodeList) {
 			this.nodes.put(n.getKey(), new NodeStructure(n));
+			lastNodeChange.put(n.getKey(),0);
 		}
 		for(EdgeData e: edgeList) {
 			this.nodes.get(e.getSrc()).addChild(e.getDest(),e);
 			this.nodes.get(e.getDest()).addParent(e.getSrc(),e);
 			this.edgeSize++;
 		}
-		for(Map.Entry<Integer, NodeStructure> entry: this.nodes.entrySet()) {
-			this.edgeMap.put(entry.getKey(), entry.getValue().getChildren().values());
-		}
+		
 		
 	}
 	
@@ -174,44 +172,166 @@ public class Graph implements DirectedWeightedGraph {
 
 	@Override
 	public void addNode(NodeData n) {
-		this.graphChanged = true;
+		
 		this.nodes.put(n.getKey(), new NodeStructure(n));
+		this.lastNodeChange.put(n.getKey(), 0);
+		this.lastGraphChange++;
 	}
 
 	@Override
 	public void connect(int src, int dest, double w) {
 		if(this.nodes.containsKey(src) && this.nodes.containsKey(dest) && !this.nodes.get(src).getChildren().containsKey(dest)) {
-			this.graphChanged = true;
 			this.edgeSize++;
 			Edge e = new Edge(src,dest,w);
 			this.nodes.get(src).addChild(dest, e);
 			this.nodes.get(dest).addParent(src, e);
+			this.lastNodeChange.put(src, lastNodeChange.get(src)+1);
+			this.lastNodeChange.put(dest, lastNodeChange.get(dest)+1);
+			this.lastEdgeChange++;
 		}
 	}
 
 	@Override
 	public Iterator<NodeData> nodeIter() throws RuntimeException{
-		if(this.graphChanged) {
-			throw new RuntimeException();
-		}
-		return this.nodeList.iterator();
+		Iterator<NodeData> iter = new Iterator<NodeData>() {
+			private Iterator<Integer> it = nodes.keySet().iterator();
+			private int changeStamp = lastGraphChange;
+			int idx;
+			private void validate() throws RuntimeException{
+				if(this.changeStamp != lastGraphChange) {
+					throw new RuntimeException();
+				}
+			}
+			@Override
+			public boolean hasNext() {
+				validate();
+				return it.hasNext();
+			}
+			@Override
+			public NodeData next() {
+				validate();
+				idx = it.next();
+				return nodes.get(idx).get();
+			}
+			
+			@Override
+			public void remove() {
+				validate();
+				removeEdge(idx);
+				lastNodeChange.remove(idx);
+				this.changeStamp = lastGraphChange;
+				it.remove();
+				
+			}
+			@Override
+			public void forEachRemaining(Consumer<? super NodeData> action) {
+				it.forEachRemaining( idx ->  action.accept(nodes.get(idx).get()));
+			}
+			
+		};
+		
+		return iter;
 	}
 
 	@Override
 	public Iterator<EdgeData> edgeIter() throws RuntimeException{
-		if(this.graphChanged) {
-			throw new RuntimeException();
-		}
-		return this.edgeList.iterator();
+		Iterator<EdgeData> iter = new Iterator<EdgeData>() {
+			private Iterator<NodeData> nodeIt = nodeIter();
+			private int src;
+			private Iterator<EdgeData> edgeIt = null;
+			private int changeStamp = lastEdgeChange;
+			
+			private void validate() throws RuntimeException{
+				if(this.changeStamp != lastEdgeChange) {
+					throw new RuntimeException();
+				}
+			}
+			@Override
+			public boolean hasNext() {
+				validate();
+				if(this.nodeIt.hasNext())
+					return this.nodeIt.hasNext();
+				return this.edgeIt != null && this.edgeIt.hasNext();
+			}
+			@Override
+			public EdgeData next() {
+				validate();
+				while(this.nodeIt.hasNext() && this.edgeIt == null || !this.edgeIt.hasNext()) {
+					this.src = nodeIt.next().getKey();
+					this.edgeIt = edgeIter(this.src);
+				}
+				return this.edgeIt.next();
+			}
+			
+			@Override
+			public void remove() {
+				validate();
+				this.edgeIt.remove();
+				this.changeStamp = lastEdgeChange;
+				
+			}
+			@Override
+			public void forEachRemaining(Consumer<? super EdgeData> action) {
+				validate();
+				while(hasNext()) {
+					action.accept(next());
+				}
+					
+					
+			}
+		};
+		return iter;
 	}
 
 	@Override
 	public Iterator<EdgeData> edgeIter(int node_id) throws RuntimeException{
-		Collection<EdgeData> collect = this.nodes.get(node_id).getChildren().values();
-		if(!collect.equals(this.edgeMap.get(node_id))) {
-			throw new RuntimeException();
-		}
-		return collect.iterator();
+		Iterator<EdgeData> iter = new Iterator<EdgeData>() {
+			private int src = node_id;
+			private int idx;
+			private Iterator<Integer> edgeIt = nodes.get(src).getChildren().keySet().iterator();
+			private int changeStamp = lastNodeChange.get(src);
+			
+			private void validate() throws RuntimeException{
+				if(this.changeStamp != lastNodeChange.get(src)) {
+					throw new RuntimeException();
+				}
+			}
+			@Override
+			public boolean hasNext() {
+				validate();
+				return edgeIt.hasNext();
+			}
+			@Override
+			public EdgeData next() {
+				validate();
+				idx = edgeIt.next();
+				return nodes.get(src).getChildren().get(idx);
+			}
+			
+			@Override
+			public void remove() {
+				validate();
+				nodes.get(idx).getChildren().remove(src);
+				edgeSize--;
+				lastNodeChange.put(src, lastNodeChange.get(src)+1);
+				lastNodeChange.put(idx, lastNodeChange.get(idx)+1);
+				lastEdgeChange++;
+				changeStamp = lastNodeChange.get(src);
+				edgeIt.remove();
+				
+				
+			}
+			@Override
+			public void forEachRemaining(Consumer<? super EdgeData> action) {
+				edgeIt.forEachRemaining(dest -> 
+					action.accept(nodes.get(src).getChildren().get(dest))
+					
+				);
+				
+					
+			}
+		};
+		return iter;
 	}
 
 	@Override
@@ -219,7 +339,12 @@ public class Graph implements DirectedWeightedGraph {
 		if(!this.nodes.containsKey(key))
 			return null;
 		
-		this.graphChanged = true;
+		removeEdge(key);
+		this.lastNodeChange.remove(key);
+		this.lastGraphChange++;
+		return this.nodes.remove(key).get();
+	}
+	private void removeEdge(int key) {
 		Set<Integer> nodeParents = this.nodes.get(key).getParents().keySet();
 		Set<Integer> nodeChildren = this.nodes.get(key).getChildren().keySet();
 		for(Integer src: nodeParents) {
@@ -228,16 +353,17 @@ public class Graph implements DirectedWeightedGraph {
 		for(Integer dest: nodeChildren) {
 			removeEdge(key,dest);
 		}
-		
-		return this.nodes.remove(key).get();
 	}
 
 	@Override
 	public EdgeData removeEdge(int src, int dest) {
 		if(this.nodes.containsKey(src) && this.nodes.containsKey(dest) && this.nodes.get(src).getChildren().containsKey(dest)) {
-			this.graphChanged = true;
+			
 			this.nodes.get(src).getChildren().remove(dest);
 			this.edgeSize--;
+			this.lastNodeChange.put(src, lastNodeChange.get(src)+1);
+			this.lastNodeChange.put(dest, lastNodeChange.get(dest)+1);
+			this.lastEdgeChange++;
 			return this.nodes.get(dest).getParents().remove(src);
 		}
 		
@@ -257,43 +383,29 @@ public class Graph implements DirectedWeightedGraph {
 	@Override
 	public int getMC() {
 		int maxEdges = 0;
-		//int maxEdgesIdx = 0;
+		
 		//Find the node with the highest amount of children and parents 
 		for(Map.Entry<Integer, NodeStructure> entry: this.nodes.entrySet()) {
 			if(maxEdges < entry.getValue().getChildren().size() + entry.getValue().getParents().size()) {
 				maxEdges = entry.getValue().getChildren().size() + entry.getValue().getParents().size();
-				//maxEdgesIdx = entry.getKey();
+				
 			}
 		}
 		return maxEdges;
 	}
 	
-	/**
-	 * updates the graph to apply the changes to it
-	 */
-	public void update() {
-		LinkedList<EdgeData> edge_list = new LinkedList<EdgeData>();
-		LinkedList<NodeData> node_list = new LinkedList<NodeData>();
-		for(NodeStructure node: this.nodes.values()) {
-			for(EdgeData edge: node.getChildren().values()) {
-				if(!edge_list.contains(edge)) {
-					edge_list.add(edge);
-				}
-			}
-			node_list.add(node.get());
-		}
-		init(node_list,edge_list);
-	}
+	
 	@Override
 	public String toString() {
 		ArrayList<String> edge_arr = new ArrayList<String>();
 		ArrayList<String> node_arr = new ArrayList<String>();
-		update();
-		for(EdgeData e: this.edgeList) {
-			edge_arr.add(e.toString());
+		Iterator<NodeData> nodeIter = nodeIter();
+		Iterator<EdgeData> edgeIter = edgeIter();
+		while(edgeIter.hasNext()) {
+			edge_arr.add(edgeIter.next().toString());
 		}
-		for(NodeData n: this.nodeList) {
-			node_arr.add(n.toString());
+		while(nodeIter.hasNext()) {
+			node_arr.add(nodeIter.next().toString());
 		}
 		HashMap<String,String> out = new HashMap<String,String>();
 		out.put("Edges", edge_arr.toString());
--- src/tests/testGraph.java
+++ src/tests/testGraph.java
@@ -78,64 +78,205 @@ class testGraph {
 	}
 	
 	@Test
-	void test_NodeOperations() {
-		 GeoLocation p1 = new Point(1,2,0);
-		 GeoLocation p2 = new Point(7,4,0);
-		 GeoLocation p3 = new Point(8,3,0);
-		 GeoLocation p4 = new Point(12,8,0);
-		 NodeData n1 = new Node(0,p1);
-		 NodeData n2 = new Node(4,p2);
-		 NodeData n3 = new Node(1,p3);
-		 NodeData n4 = new Node(2,p4);
-		 LinkedList<NodeData> nodes = new LinkedList<NodeData>();
-		 LinkedList<EdgeData> edges = new LinkedList<EdgeData>();
-		 nodes.add(n1);
-		 nodes.add(n2);
+	void test_NodeIterator() {
+		GeoLocation p1 = new Point(1,2,0);
+		GeoLocation p2 = new Point(7,4,0);
+		GeoLocation p3 = new Point(8,3,0);
+		NodeData n1 = new Node(0,p1);
+		NodeData n2 = new Node(4,p2);
+		NodeData n3 = new Node(1,p3);
+		LinkedList<NodeData> nodes = new LinkedList<NodeData>();
+		LinkedList<EdgeData> edges = new LinkedList<EdgeData>();
+		nodes.add(n1);
+		nodes.add(n2);
+		
+		DirectedWeightedGraph g = new Graph(nodes,edges);
+		Iterator<NodeData> nodeIter = g.nodeIter();
+		int counter = 0;
+		//test iterator
+		while(nodeIter.hasNext()) {
+			assertTrue(nodes.remove(nodeIter.next()));
+			counter++;
+		}
+		assertTrue(counter == 2);
+		assertTrue(g.nodeSize()==2);//testing node size 
+		nodeIter = g.nodeIter();
+		//testing add node
+		g.addNode(n3);
+		assertTrue(g.nodeSize()==3);//testing node size gets updated
+		//test that after changes to the graph are made, we cant use any iterators made before the change
+		try {
+			nodeIter.hasNext();
+			assertTrue(false);
+		}
+		catch(RuntimeException e){
+			assertTrue(true);
+		}
+		nodeIter = g.nodeIter();
+		System.out.println("should print 3 node id's:");
+		nodeIter.forEachRemaining(o -> System.out.println(o.getKey()));
+		
+		
+		nodeIter = g.nodeIter();
+		nodeIter.next();
+		System.out.println("should print 2 node id's:");
+		nodeIter.forEachRemaining(o -> System.out.println(o.getKey()));
 		 
-		 DirectedWeightedGraph g = new Graph(nodes,edges);
-		 Iterator<NodeData> nodeIter = g.nodeIter();
-		 int counter = 0;
-		 //test iterator
-		 while(nodeIter.hasNext()) {
-			 assertTrue(nodes.remove(nodeIter.next()));
-			 counter++;
-		 }
-		 assertTrue(counter == 2);
-		 
-		 assertTrue(g.nodeSize()==2);//testing node size 
-		 //testing add node
-		 g.addNode(n3);
-		 
-		 assertTrue(g.nodeSize()==3);//testing node size gets updated
-		 //test that after changes to the graph are made, we cant get an iterator back
-		 try {
-			 nodeIter = g.nodeIter();
-			 assertTrue(false);
-		 }
-		 catch(RuntimeException e){
-			 assertTrue(true);
-		 }
-		 
-		 
-		 counter = 0;
-		 //test iterator
-		 while(nodeIter.hasNext()) {
-			 assertTrue(nodes.remove(nodeIter.next()));
-			 counter++;
-		 }
-		 
-		 
-		 //testing get node
-		 assertTrue(g.getNode(0).equals(n1));
-		 assertTrue(g.getNode(1).equals(n3));
-		 assertTrue(g.getNode(2).equals(n4));
-		 assertTrue(g.getNode(3) == null);
-		 assertTrue(g.getNode(4).equals(n2));
-		 
-		 
-		 
+		nodeIter = g.nodeIter();
+		nodeIter.next();
+		nodeIter.remove();
+		assertTrue(g.nodeSize()==2);//testing that iterator remove can be done more than once, and updates the graph 
+		nodeIter.next();
+		nodeIter.remove();
+		assertTrue(g.nodeSize()==1);
+		
+ 
 	}
 	
+	
+	@Test
+	void test_EdgeIterator() {
+		GeoLocation p1 = new Point(1,2,0);
+		GeoLocation p2 = new Point(7,4,0);
+		GeoLocation p3 = new Point(8,3,0);
+		GeoLocation p4 = new Point(12,8,0);
+		NodeData n1 = new Node(0,p1);
+		NodeData n2 = new Node(4,p2);
+		NodeData n3 = new Node(1,p3);
+		NodeData n4 = new Node(2,p4);
+		LinkedList<NodeData> nodes = new LinkedList<NodeData>();
+		nodes.add(n1);
+		nodes.add(n2);
+		nodes.add(n3);
+	 	nodes.add(n4);
+	 	LinkedList<EdgeData> edges = new LinkedList<EdgeData>();
+	 	EdgeData e1 = new Edge(1,4,4.15);
+	 	EdgeData e2 = new Edge(4,1,3.75);
+	 	
+	 	edges.add(e1);
+	 	edges.add(e2);
+	 	
+		
+		DirectedWeightedGraph g = new Graph(nodes,edges);
+		Iterator<EdgeData> edgeIter = g.edgeIter();
+		int counter = 0;
+		//test iterator
+		while(edgeIter.hasNext()) {
+			
+			assertTrue(edges.remove(edgeIter.next()));
+			counter++;
+		}
+		assertTrue(counter == 2);
+		assertTrue(g.edgeSize()==2);//testing edgeSize
+		edgeIter = g.edgeIter();
+		//testing connect
+		g.connect(1,2,1);
+		assertTrue(g.edgeSize()==3);//testing edge size gets updated
+		//test that after changes to the graph are made, we cant use any iterators made before the change
+		try {
+			edgeIter.hasNext();
+			assertTrue(false);
+		}
+		catch(RuntimeException e){
+			assertTrue(true);
+		}
+		edgeIter = g.edgeIter();
+		System.out.println("should print 3 edge weights:");
+		edgeIter.forEachRemaining(o -> System.out.println(o.getWeight()));
+		
+		
+		edgeIter = g.edgeIter();
+		edgeIter.next();
+		System.out.println("should print 2 edge weights:");
+		edgeIter.forEachRemaining(o -> System.out.println(o.getWeight()));
+		 
+		edgeIter = g.edgeIter();
+		edgeIter.next();
+		edgeIter.remove();
+		assertTrue(g.edgeSize()==2);//testing that iterator remove can be done more than once, and updates the graph 
+		edgeIter.next();
+		edgeIter.remove();
+		assertTrue(g.edgeSize()==1);
+		
+		
+	}
+	@Test
+	void test_EdgeIterator2() {
+		GeoLocation p1 = new Point(1,2,0);
+		GeoLocation p2 = new Point(7,4,0);
+		GeoLocation p3 = new Point(8,3,0);
+		GeoLocation p4 = new Point(12,8,0);
+		NodeData n1 = new Node(0,p1);
+		NodeData n2 = new Node(4,p2);
+		NodeData n3 = new Node(1,p3);
+		NodeData n4 = new Node(2,p4);
+		LinkedList<NodeData> nodes = new LinkedList<NodeData>();
+		nodes.add(n1);
+		nodes.add(n2);
+		nodes.add(n3);
+	 	nodes.add(n4);
+	 	LinkedList<EdgeData> edges = new LinkedList<EdgeData>();
+	 	EdgeData e1 = new Edge(1,4,4.15);
+	 	EdgeData e2 = new Edge(1,2,3.75);
+	 	EdgeData e3 = new Edge(2,1,8.3);
+	 	
+	 	edges.add(e1);
+	 	edges.add(e2);
+	 	edges.add(e3);
+		
+		DirectedWeightedGraph g = new Graph(nodes,edges);
+		Iterator<EdgeData> edgeIter = g.edgeIter(1);
+		int counter = 0;
+		//test iterator
+		while(edgeIter.hasNext()) {
+			
+			assertTrue(edges.remove(edgeIter.next()));
+			counter++;
+		}
+		assertTrue(counter == 2);
+		
+		edgeIter = g.edgeIter(1);
+		//testing connect
+		g.connect(0,2,1);
+		
+		//test that after changes to the graph are made, we can use the iterator only if the edge was unaffected
+		try {
+			edgeIter.hasNext();
+			assertTrue(true);
+		}
+		catch(RuntimeException e){
+			assertTrue(false);
+		}
+		g.connect(1, 0, 5.3);
+		//test that after changes to the graph are made, we can use the iterator only if the edge was unaffected
+		try {
+			edgeIter.hasNext();
+			assertTrue(false);
+		}
+		catch(RuntimeException e){
+			assertTrue(true);
+		}
+		
+		edgeIter = g.edgeIter(1);
+		System.out.println("should print 3 edge weights:");
+		edgeIter.forEachRemaining(o -> System.out.println(o.getWeight()));
+		
+		
+		edgeIter = g.edgeIter(1);
+		edgeIter.next();
+		System.out.println("should print 2 edge weights:");
+		edgeIter.forEachRemaining(o -> System.out.println(o.getWeight()));
+		 
+		edgeIter = g.edgeIter();
+		edgeIter.next();
+		edgeIter.remove();
+		assertTrue(g.edgeSize()==4);//testing that iterator remove can be done more than once, and updates the graph 
+		edgeIter.next();
+		edgeIter.remove();
+		assertTrue(g.edgeSize()==3);
+		
+		
+	}
 	
 
 }
