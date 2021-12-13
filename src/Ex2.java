6c5653088036427e816dade0aa4d264644ac3de8
Ziv Morgan <ziv.morgan@gmail.com>
2021-12-14 00:00:10 +0200

    added jar folder

--- src/Ex2.java
+++ src/Ex2.java
@@ -1,13 +1,17 @@
 
 
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 import api.DirectedWeightedGraph;
 import api.DirectedWeightedGraphAlgorithms;
 import api.EdgeData;
+import api.GeoLocation;
 import api.NodeData;
 import api_implementation.Algorithems;
 import api_implementation.Graph;
+import api_implementation.Point;
 import processing.core.*;
 
 /**
@@ -15,86 +19,7 @@ import processing.core.*;
  */
 public class Ex2 extends PApplet{
 	public static void main(String[] args)  {
-		/*
-		GeoLocation p1 = new Point(10,0,0);
-		GeoLocation p2 = new Point(0,10,0);
-		GeoLocation p3 = new Point(-10,0,0);
-		GeoLocation p4 = new Point(0,-10,0);
-		GeoLocation p5 = new Point(7,5,0);
-		GeoLocation p6 = new Point(-1,7,0);
-		GeoLocation p7 = new Point(-4,-3,0);
-		GeoLocation p8 = new Point(5,2,0);
-		NodeData n1 = new Node(0,p1);
-		NodeData n2 = new Node(1,p2);
-		NodeData n3 = new Node(2,p3);
-		NodeData n4 = new Node(4,p4);
-		NodeData n5 = new Node(3,p5);
-		NodeData n6 = new Node(5,p6);
-		NodeData n7 = new Node(6,p7);
-		NodeData n8 = new Node(7,p8);
-		EdgeData e1 = new Edge(0,1,p1.distance(p2));
-	 	EdgeData e2 = new Edge(1,2,p2.distance(p3));
-	 	EdgeData e3 = new Edge(2,4,p3.distance(p4));
-	 	EdgeData e4 = new Edge(4,0,p4.distance(p1));
-	 	EdgeData e5 = new Edge(3,5,p5.distance(p6));
-	 	EdgeData e6 = new Edge(5,6,p6.distance(p7));
-	 	EdgeData e7 = new Edge(6,0,p6.distance(p1));
-	 	EdgeData e8 = new Edge(3,0,p5.distance(p1));
-	 	EdgeData e9 = new Edge(2,3,p3.distance(p5));
-	 	EdgeData e10 = new Edge(2,5,1);
-	 	EdgeData e11 = new Edge(5,4,1);
-		LinkedList<NodeData> nodes = new LinkedList<NodeData>();
-		LinkedList<EdgeData> edges = new LinkedList<EdgeData>();
-		nodes.add(n1);
-		nodes.add(n2);
-		nodes.add(n3);
-		nodes.add(n4);
-		nodes.add(n5);
-		nodes.add(n6);
-		nodes.add(n7);
-		nodes.add(n8);
-		
-		edges.add(e1);
-		edges.add(e2);
-		edges.add(e3);
-		edges.add(e4);
-		edges.add(e5);
-		edges.add(e6);
-		edges.add(e7);
-		edges.add(e8);
-		edges.add(e9);
-		edges.add(e10);
-		edges.add(e11);
-		
-		*/
-		/*
-		LinkedList<NodeData> nodes = new LinkedList<NodeData>();
-		LinkedList<EdgeData> edges = new LinkedList<EdgeData>();
-		for(int i = 0; i<10000; i++) {
-			double x = Math.random()*(900);
-			double y = Math.random()*(900);
-			double z = Math.random()*(900);
-			GeoLocation p = new Point(x,y,z);
-			nodes.add(new Node(i,p));
-		}
-		for(int n = 0; n<10000; n++) {
-			for(int ed = 0; ed<15; ed++) {
-				if(Math.random() > 0.5) {
-					edges.add(new Edge(n,(int) Math.floor(Math.random()*nodes.size()), Math.random()*200));
-				}
-			}
-		}
-		System.out.println("made params");
-		DirectedWeightedGraph g = new Graph(nodes,edges);
-		System.out.println("init graph");
-		DirectedWeightedGraphAlgorithms alg = new Algorithems(g);
-		System.out.println("init Algorithm");
-		
-		System.out.println(alg.save("data/"+name+".json"));
-		System.out.println(alg.isConnected());
-		
-		*/
-		String name = "2-5-4faster";
+	
 		
 		
 		
@@ -102,7 +27,7 @@ public class Ex2 extends PApplet{
 			runGUI("data/"+args[0]);
 		}
 		else {
-			runGUI("data/"+name+".json");
+			runGUI("data/G1.json");
 		}
 		
 		
@@ -150,7 +75,11 @@ public class Ex2 extends PApplet{
     DirectedWeightedGraphAlgorithms alg;
     double scaleX;
     double scaleY;
+    boolean multi_select = false;
+    int choose_2 = 2;
     double minX = Double.MAX_VALUE, minY = Double.MAX_VALUE;
+    List<NodeData> marked_nodes;
+    List<EdgeData> marked_edges;
     //float scaleZ;
     
     @Override
@@ -162,6 +91,8 @@ public class Ex2 extends PApplet{
     public void setup() {
     	scaleX = 1;
     	scaleY = 1;
+    	marked_nodes = new  LinkedList<NodeData>();
+    	marked_edges = new  LinkedList<EdgeData>();
     	//scaleZ = 1;
     	alg = getGrapgAlgo(args[0]);
     	preProcessGraph();
@@ -170,7 +101,9 @@ public class Ex2 extends PApplet{
     @Override
     public void draw() {
     	background(255);
-    	
+    	double mX = (mouseX)/scaleX + minX;
+    	double mY = (mouseY)/scaleY + minY;
+    	text(mX + ", " + mY, mouseX,mouseY );
     	translate( (float)Math.max((width*0.9-height*0.9)/2,0),(float)Math.max((height*0.9-width*0.9)/2,0));
     	
     	pushMatrix();
@@ -185,39 +118,36 @@ public class Ex2 extends PApplet{
     		stroke(0);
     		NodeData node = nodeIter.next();
     		pushMatrix();
-    		textAlign(LEFT);
-    		textSize((float)(20/Math.min(scaleX,scaleY)));
-    		text(node.getKey(),(float)(((20/scaleX))+node.getLocation().x()-minX), (float)(-((20/scaleY))+node.getLocation().y()-minY));
-    		
-    		
+    		if(marked_nodes.contains(node)) {
+    			fill(0,255,0);
+    		}
     		translate((float)(node.getLocation().x()-minX),(float)(node.getLocation().y()-minY));
     		float size = 20;
     		ellipse((float)0,(float)0,(float)(size/scaleX),(float)(size/scaleY));
-    		
     		popMatrix();
-    		
     	}
-    	
     	Iterator<EdgeData> edgeIter =  alg.getGraph().edgeIter();
     	while(edgeIter.hasNext()) {
     		fill(0);
-    		
+    		stroke(0);
     		EdgeData edge = edgeIter.next();
     		NodeData node1 = alg.getGraph().getNode(edge.getSrc());
     		NodeData node2 = alg.getGraph().getNode(edge.getDest());
+    		if(marked_edges.contains(edge)) {
+    			fill(0,255,0);
+    			stroke(0,255,0);
+    		}
     		float x1 = (float)(node1.getLocation().x()-minX);
     		float x2 = (float)(node2.getLocation().x()-minX);
     		float y1 = (float)(node1.getLocation().y()-minY);
     		float y2 = (float)(node2.getLocation().y()-minY);
     		line(x1,y1,x2,y2);
-    		
     	}
     	popMatrix();
 
     	edgeIter =  alg.getGraph().edgeIter();
     	while(edgeIter.hasNext()) {
     		fill(0);
-    		//strokeWeight(max((float) (scaleX/10), (float)(scaleY/10)) );
     		EdgeData edge = edgeIter.next();
     		NodeData node1 = alg.getGraph().getNode(edge.getSrc());
     		NodeData node2 = alg.getGraph().getNode(edge.getDest());
@@ -232,24 +162,72 @@ public class Ex2 extends PApplet{
     		double newAngle = -1/angle;
     		double tempY1 = newAngle + intersection[1];
     		double tempY2 = -newAngle + intersection[1];
-    		double[] point1= getPoint(intersection[0],intersection[1],intersection[0]+1,tempY1, Math.max(scaleX,scaleY)*0.4*distance/Math.pow(3, 2) );
-    		double[] point2= getPoint(intersection[0],intersection[1],intersection[0]-1,tempY2, Math.max(scaleX,scaleY)*0.4*distance/Math.pow(3, 2) );
+    		double[] point1= getPoint(intersection[0],intersection[1],intersection[0]+1,tempY1, ((scaleX+scaleY)/2)*0.4*distance/Math.pow(3, 2) );
+    		double[] point2= getPoint(intersection[0],intersection[1],intersection[0]-1,tempY2, ((scaleX+scaleY)/2)*0.4*distance/Math.pow(3, 2) );
     		stroke(255,0,0);
     		fill(255,0,0);
-    		
-    		
-    		
     		triangle(x2,y2,(float)(point1[0]),(float)point1[1],(float)point2[0],(float)point2[1]);
-    		
-    		
-    		
- 
-    		
-    		
     	}
     	popMatrix();
     	
     }
+    public void keyReleased() {
+    	if(key=='c') {
+    		marked_nodes = new  LinkedList<NodeData>();
+    		marked_edges = new  LinkedList<EdgeData>();
+    		marked_nodes.add(alg.center());
+    	}
+    	if(key == 't') {
+    		multi_select = !multi_select;
+    		if(multi_select) {
+    			marked_nodes = new  LinkedList<NodeData>();
+        		marked_edges = new  LinkedList<EdgeData>();
+    		}
+    		else {
+    			marked_nodes = alg.tsp(marked_nodes);
+    			marked_edges = new  LinkedList<EdgeData>();
+    			for(int i = 0; i<marked_nodes.size()-1; i++) {
+    				marked_edges.add(alg.getGraph().getEdge(marked_nodes.get(i).getKey(), marked_nodes.get(i+1).getKey()));
+    			}
+    		}
+    	}
+    	if(key == 'p') {
+    		marked_nodes = new  LinkedList<NodeData>();
+    		marked_edges = new  LinkedList<EdgeData>();
+    		choose_2 = 0;
+    	}
+    }
+    public void mousePressed() {
+    	double dist = Double.MAX_VALUE;
+    	NodeData n = null;
+    	
+    	double mX = (mouseX)/scaleX + minX - (float)Math.max((width*0.9-height*0.9)/2,0);
+    	double mY = (mouseY)/scaleY + minY - (float)Math.max((width*0.9-height*0.9)/2,0);
+    	ellipse((float)mX,(float)mY,20,20);
+    	GeoLocation mouse = new Point(mX,mY,0);
+    	Iterator<NodeData> nodeIter =  alg.getGraph().nodeIter();
+    	while(nodeIter.hasNext()) {
+    		NodeData node = nodeIter.next();
+    		if(dist > node.getLocation().distance(mouse)) {
+    			n = node;
+    			dist = node.getLocation().distance(mouse);
+    		}
+    	}
+    	
+    	if(multi_select) {
+    		if(!marked_nodes.contains(n)) {
+    			marked_nodes.add(n);
+    		}
+    	}
+    	if(choose_2<2) {
+    		if(!marked_nodes.contains(n)) {
+    			marked_nodes.add(n);
+    			choose_2++;
+    		}
+    	}
+    }
+    
+    
     double[] getPoint(double x1,double y1,double x2,double y2,double distance) {
 		double vectX = x2-x1;
 		double vectY = y2-y1;
@@ -259,8 +237,6 @@ public class Ex2 extends PApplet{
 		double pY = y1+uY*distance;
 		return new double[] {pX,pY};
     }
-    
-    
     void preProcessGraph() {
     	Iterator<NodeData> nodeIter =  alg.getGraph().nodeIter();
     	double  maxX = Float.MIN_VALUE, maxY = Float.MIN_VALUE; 
@@ -269,18 +245,13 @@ public class Ex2 extends PApplet{
     		maxX = Math.max(maxX, node.getLocation().x());
     		maxY = Math.max(maxY, node.getLocation().y());
     		minX = Math.min(minX, node.getLocation().x());
-    		minY = Math.min(minY, node.getLocation().y());
-    		
+    		minY = Math.min(minY, node.getLocation().y());	
     	}
-    	
     	scaleX = (maxX - minX);
     	scaleY = (maxY - minY);
     	scaleX = ((width*0.8)/scaleX);
     	scaleY = ((height*0.8)/scaleY);
     	nodeIter =  alg.getGraph().nodeIter();
-    	
-    	
-    
     }
     
     
